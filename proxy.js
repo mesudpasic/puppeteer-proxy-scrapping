108e4add5b394ccbfeaa20a3d0d80c4aaad3b604
mesud <839950+mesudpasic@users.noreply.github.com>
2020-11-26 11:55:37 +0100

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -6,28 +6,25 @@ const PROXY_SITE="https://free-proxy-list.net/";
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();    
     await page.goto(PROXY_SITE);
-    let proxyList =await page.evaluate(() => {        
-        let rc=1; //row count, we need to skip first one here since it's heading
+    let proxyList =await page.evaluate(() => {                
         let list=[];
         let currPageIndex=0; /* starting point */
         let currPage=document.querySelector(".paginate_button.active a").innerText;
         try{        
             while (currPageIndex<currPage){
                 /* we select all table rows from current paginated dataset and look for table data cells */
-                document.querySelectorAll(".dataTable tr").forEach(row => {
-                    if (rc>1){
-                        let proxyItem={};                    
-                        /* get all cell elements in array */
-                        let td=row.getElementsByTagName("td");                      
-                        if (td.length){
-                            /* ip is first cell, 2nd is port number, and 6th cell is indicator if proxy supports HTTPS */          
-                            proxyItem.ip=td[0].innerText;
-                            proxyItem.port=td[1].innerText;
-                            proxyItem.ssl=td[6].innerText==="no" ? false : true;                            
-                            list.push(proxyItem);
-                        }
-                    }                
-                    rc++;                
+                document.querySelectorAll(".dataTable tr").forEach(row => {                    
+                    let proxyItem={};                    
+                    /* get all cell elements in array */
+                    let td=row.getElementsByTagName("td");                      
+                    /* make sure length>0 otherwise it could be that we are dealing with header or footer th elements */
+                    if (td.length){
+                        /* ip is first cell, 2nd is port number, and 6th cell is indicator if proxy supports HTTPS */          
+                        proxyItem.ip=td[0].innerText;
+                        proxyItem.port=td[1].innerText;
+                        proxyItem.ssl=td[6].innerText==="no" ? false : true;                            
+                        list.push(proxyItem);
+                    }                    
                 });
                 currPageIndex++;
                 /* simulate 'Next' click */
